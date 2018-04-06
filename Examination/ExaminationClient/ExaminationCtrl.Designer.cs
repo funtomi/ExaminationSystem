7461c803d1f471a0c53b17810b73972a292be20f
funtomi <396166153@qq.com>
2018-04-06 16:29:33 +0800

    考试界面

--- Examination/ExaminationClient/ExaminationCtrl.Designer.cs
+++ Examination/ExaminationClient/ExaminationCtrl.Designer.cs
@@ -23,17 +23,149 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmboxSubSeq = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSubNum = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lalTime = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(30, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(860, 367);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(806, 477);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 40);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "交卷";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(815, 56);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "下一题";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreview.Location = new System.Drawing.Point(697, 56);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 4;
+            this.btnPreview.Text = "上一题";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(607, 493);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "第";
+            // 
+            // cmboxSubSeq
+            // 
+            this.cmboxSubSeq.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmboxSubSeq.FormattingEnabled = true;
+            this.cmboxSubSeq.Location = new System.Drawing.Point(629, 487);
+            this.cmboxSubSeq.Name = "cmboxSubSeq";
+            this.cmboxSubSeq.Size = new System.Drawing.Size(55, 23);
+            this.cmboxSubSeq.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(722, 492);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "题";
+            // 
+            // lblSubNum
+            // 
+            this.lblSubNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSubNum.AutoSize = true;
+            this.lblSubNum.Location = new System.Drawing.Point(691, 492);
+            this.lblSubNum.Name = "lblSubNum";
+            this.lblSubNum.Size = new System.Drawing.Size(31, 15);
+            this.lblSubNum.TabIndex = 8;
+            this.lblSubNum.Text = "/20";
+            this.lblSubNum.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(694, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "剩余时间：";
+            // 
+            // lalTime
+            // 
+            this.lalTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lalTime.AutoSize = true;
+            this.lalTime.ForeColor = System.Drawing.Color.White;
+            this.lalTime.Location = new System.Drawing.Point(769, 20);
+            this.lalTime.Name = "lalTime";
+            this.lalTime.Size = new System.Drawing.Size(0, 15);
+            this.lalTime.TabIndex = 10;
             // 
             // ExaminationCtrl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.Controls.Add(this.lalTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSubNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmboxSubSeq);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.panel1);
             this.Name = "ExaminationCtrl";
-            this.Size = new System.Drawing.Size(975, 615);
+            this.Size = new System.Drawing.Size(924, 537);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmboxSubSeq;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSubNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lalTime;
     }
 }
